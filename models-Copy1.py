808479741c5ef67edca8bde7dcea534f53e12471
Vinayraj Singh <vinayrraj@gmail.com>
2018-11-24 20:13:19 +0530

    Facial Keypoints recognition project completion

---
+++ models-Copy1.py
@@ -0,0 +1,135 @@
+## TODO: define the convolutional neural network architecture
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+# can use the below import should you choose to initialize the weights of your Net
+import torch.nn.init as I
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        '''
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        # output size = (W - F)/S + 1 = (224 - 5)/1 + 1 = 220
+        # output tensor: (32, 220, 220)
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        I.xavier_uniform_(self.conv1.weight, gain=np.sqrt(2.0))
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        # > relu or (ELUs) (32, 220, 220)
+        # output tensor: (32, 110, 110)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout1 = nn.Dropout(p=0.3)
+        # output tensor: (64, 108, 108)
+        
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        I.xavier_uniform_(self.conv2.weight, gain=np.sqrt(2.0))
+        # ELUs(64, 108, 108)-> pool (64, 54, 54)-> dropout(0.4)(64, 54, 54)
+        self.dropout2 = nn.Dropout(p=0.5)
+        
+        # output tensor: (128, 53, 53)
+        self.conv3 = nn.Conv2d(64, 128, 2)
+        I.xavier_uniform_(self.conv3.weight, gain=np.sqrt(2.0))
+        # ELUs(128, 53, 53)-> pool (128, 26, 26)-> dropout(0.4)(128, 26, 26)
+        self.dropout3 = nn.Dropout(p=0.4)
+        
+        # faltten 256*12*12 -> 2000 -> Relu -> dropout (0.4)
+        self.fc1 = nn.Linear(128*26*26, 2000, bias=True)
+        I.kaiming_uniform_(self.fc1.weight)
+        I.constant(self.fc1.bias, 0.1)
+        self.dropout5 = nn.Dropout(p=0.4)
+        # Relu => dropout with 0.4
+        
+        self.fc2 = nn.Linear(2000, 2000, bias=True)
+        self.dropout6 = nn.Dropout(p=0.4)
+        I.kaiming_uniform_(self.fc2.weight)
+        I.constant(self.fc2.bias, 0.1)
+        # finally, create 68 * 2 output keypoints
+        self.fc3 = nn.Linear(2000, 68*2)
+        I.kaiming_uniform_(self.fc3.weight)
+        I.constant(self.fc3.bias, 0.1)
+'''
+        
+        super(Net, self).__init__()
+        
+        # Convolution Layers
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+        self.conv4 = nn.Conv2d(128, 256, 5)
+
+        # Maxpooling Layers
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        # Fully Connected Layers
+        self.fc1 = nn.Linear(25600, 2048)
+        self.fc2 = nn.Linear(2048, 1024)
+        self.fc3 = nn.Linear(1024, 136)
+        
+        # Dropout Layers
+        self.drop = nn.Dropout(0.5)
+        
+        
+        
+        
+        
+
+        
+    def forward(self, x):
+        '''
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        ## x = self.pool(F.relu(self.conv1(x)))
+        # output tensor: (32, 110, 110)
+        x = self.dropout1(self.pool(F.relu(self.conv1(x))))
+        # output tensor: (64, 54, 54)
+        x = self.dropout2(self.pool(F.relu(self.conv2(x))))
+        # output tensor: (128, 26, 26)
+        x = self.dropout3(self.pool(F.relu(self.conv3(x))))
+        # output tensor: (256, 12, 12)
+        # x = self.dropout4(self.pool(F.relu(self.conv4(x))))
+        # prep for linear layer
+        x = x.view(x.size(0), -1)
+        # three linear layers with dropout in between
+        x = self.dropout5(F.relu(self.fc1(x)))
+        x = self.dropout6(F.relu(self.fc2(x)))
+        # final output
+        x = self.fc3(x)
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
+        '''
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        x = self.drop(x)
+        
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.drop(x)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.drop(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.drop(x)
+        
+        x = x.view(x.size(0), -1)
+        
+        x = self.drop(F.relu(self.fc1(x)))
+        x = self.drop(F.relu(self.fc2(x)))
+        x = self.fc3(x)
+        #x = F.log_softmax(x, dim=1)
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
+
